0990d0160be084b65a468ee726631de663c0e486
DanielAharon <danielaharon53@gmail.com>
2025-05-06 14:23:01 +0300

    Setting everything back to normal

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name__)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello YIGANNNNNNN!</p>"
 
 if __name__ == "__main__":
-    app.ru34234n(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
