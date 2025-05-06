fa0f49e768b3b104db95c6f443d229394a591444
DanielAharon <danielaharon53@gmail.com>
2025-05-06 14:15:06 +0300

    experimenting

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello YIGANNNNNNN!</p>"
 
 if __name__ == "__main__":
-    app.ru33423423n(host="0.0.0.0", port=5000, debug=True)
+    app.ru34234n(host="0.0.0.0", port=5000, debug=True)
